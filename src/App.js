b737561b40414b26dbf62e77e3efffc871a35000
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-02 12:53:24 +0600

    inital setup for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import { Button, Spinner } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const design = {
+    color:"red",
+    width:'400px',
+    backgroundColor:"green"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="hello"> Hello world</h1>
+      <ExternalUser> </ExternalUser>
+     
+      
+    </div>
+  );
+}
+
+function ExternalUser() {
+  const [user, setUser] = useState([]) ;
+  
+
+
+  return (
+    <div className="hello">
+      <h2>ExternalUser</h2>
     </div>
   );
 }
