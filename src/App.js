d956c5d08f5895ad0f1cafc5a0b2fda3da097f2f
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-02-04 09:24:15 +0600

    timer added

--- src/App.js
+++ src/App.js
@@ -13,11 +13,41 @@ function App() {
     <div className="App">
       <h1 className="hello"> Hello world</h1>
       <ExternalUser> </ExternalUser>
+      <Counter> </Counter>
+
      
       
     </div>
   );
 }
+
+const Counter = () => {
+
+const [count, setCount] = useState(0);
+const handleIncrease = () => setCount(count + 1);
+  const handleDecrise = () => setCount(count - 1);
+  
+
+  return(
+       <div className="hello" >
+         <h2>Counter : {count}</h2>
+         <button onClick={handleIncrease}>+1</button><br /> <br />
+         <button onClick={handleDecrise}>-1</button>
+       </div>
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
 
 function ExternalUser() {
  /*  const [user, setUser] = useState([]) ;
@@ -27,20 +57,20 @@ function ExternalUser() {
     .then(data => console.log(data) )
   },[]) */
 
-  const [comments, setComments] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(()=> {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json())
-    .then(data => setComments(data))
+    .then(data => setUsers(data))
   },[]);
 
 
   return (
     <div className="hello">
-      <h2>ExternalUser: {comments.length}</h2>
+      <h2>ExternalUser: {users.length}</h2>
       {
-        comments.map( comment => <li style={{marginTop: "30px"}}> {comment.title}</li>)
+        users.map( user => <li style={{marginTop: "30px"}}> {user.name}</li>)
       }
     </div>
   );
